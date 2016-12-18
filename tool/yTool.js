fc58d4540d9f45bbc380c979397b87747812a3a3
yyc <395976266@qq.com>
2016-12-18 10:30:32 +0800

    modify Tool.event->triggerEvent: support pass eventData

--- tool/yTool.js
+++ tool/yTool.js
@@ -1557,7 +1557,7 @@
              }
 
              */
-            triggerEvent: function (oTarget, type) {
+            triggerEvent: function (oTarget, type, eventData) {
                 var evObj = null,
                     dom = null;
 
@@ -1617,6 +1617,11 @@
                     //此处使用通用事件
                     evObj = document.createEvent('Events');
                     evObj.initEvent(type, false, true);
+
+                    if(!!eventData){
+                        Tool.extend.extend(evObj, eventData);
+                    }
+
                     if (Tool.judge.isjQuery(oTarget)) {
                         oTarget.each(function () {
                             dom = this;
